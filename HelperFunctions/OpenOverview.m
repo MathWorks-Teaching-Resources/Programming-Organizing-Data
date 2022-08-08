9ee2eed2e8bbfd3f95736bc60eff7e09143ba922
esmithzb <esmithzb@mathworks.com>
2022-08-08 14:55:31 -0400

    Update the OpenOverview to minimize code
    1) Make sure OpenOverview calls either Overview.html or Navigation.mlx
    2) Make sure OpenOverview doesn't open and fail to close
    3) Put both Overview and Navigation in HelperFunctions

--- HelperFunctions/OpenOverview.m
+++ HelperFunctions/OpenOverview.m
@@ -2,12 +2,12 @@ function OpenOverview
 % Open the overview file
 locDir = pwd;
 if contains(locDir,filesep+"MATLAB Drive")
-    open("NavigationOverview.mlx")
+    open("Navigation.mlx")
 else
     open("Overview.html")
 end
 
-% Close the current script
-open("OpenOverview.m")
-close(matlab.desktop.editor.getActive)
+% % Close the current script
+% open("OpenOverview.m")
+% close(matlab.desktop.editor.getActive)
 end
